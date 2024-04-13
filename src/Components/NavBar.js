75f9b4f6254603c83388b9c33c3f5e2d17fc3c99
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-13 20:27:16 +0500

    Add files via upload

---
+++ src/Components/NavBar.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import { FaFacebookF, FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
+
+
+const actions = [
+    { icon: <FaFacebookF color='blue' />, name: 'Facebook', href: 'https://www.facebook.com/abdullah.tariq.7262' },
+    { icon: <FaGithub color='black'/>, name: 'GitHub', href: 'https://github.com/Abdullahprogramme' },
+    { icon: <FaLinkedinIn color='blue'/>, name: 'LinkedIn', href: 'https://www.linkedin.com/in/abdullahtariq78/' },
+    { icon: <FaEnvelope color='red'/>, name: 'Email', href: 'mailto:abdtariq78@gmail.com' },
+];
+  
+export default function NavBar() {
+    return (
+        <SpeedDial
+            ariaLabel="SpeedDial"
+            sx={{ position: 'absolute', bottom: 16, right: 16 }}
+            icon={<SpeedDialIcon />}
+        >
+        {actions.map((action) => (
+            <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={(event) => {
+                event.preventDefault();
+                window.open(action.href, '_blank', 'noopener,noreferrer');
+            }}
+            />
+        ))}
+        </SpeedDial>
+    );
+}
