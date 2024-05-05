3c003ff5c3bafc23f67e458f199778c28f9a4623
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-05-05 15:42:44 +0500

    Update Recommender.js

--- src/Pages/Recommender.js
+++ src/Pages/Recommender.js
@@ -1,13 +1,30 @@
 import React from "react";
-import NavBar from "../Components/NavBar";
 import Header from "../Components/Header";
-
+import { Alert } from "@mui/material";
+import { useTheme, useMediaQuery } from "@mui/material";
 
 export default function Recommender() {
+    const theme = useTheme();
+    const isScreenSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
         <div>
             <Header />
-            <NavBar />
+
+            <Alert 
+                severity="info" 
+                sx={{ 
+                    textAlign: 'center', 
+                    margin: 'auto', 
+                    width: isScreenSmall ? '310px' : '600px',
+                    marginTop: 3,
+                    justifyContent: 'center',
+                    alignItems: 'center' 
+                }}
+            >
+            FIll the form in the Recommendation page and see the recommendations here.
+            </Alert>
+           
         </div>
     );
 }
