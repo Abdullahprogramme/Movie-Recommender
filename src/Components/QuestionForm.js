51c33a74a2947c5a5e26f94d43275c1efbe41fe1
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-05-05 23:46:08 +0500

    Update QuestionForm.js

--- src/Components/QuestionForm.js
+++ src/Components/QuestionForm.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Checkbox, FormControlLabel, Box, Typography } from '@mui/material';
+import { useTheme, useMediaQuery } from '@mui/material';
 
 export default function QuestionForm({ title, options, onOptionToggle }) {
   const optionsPerColumn = 4;
   const firstColumnOptions = options.slice(0, optionsPerColumn);
   const secondColumnOptions = options.slice(optionsPerColumn);
 
+
+  const theme = useTheme();
+  const isScreenSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Box>
+    <Box sx={{ marginTop: isScreenSmall ? 0 : 6 }}>
       <Typography variant="h6" gutterBottom style={{ textAlign: 'center' }}>
         {title}
       </Typography>
       <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '10px' }}>
           {firstColumnOptions.map((option, index) => (
             <FormControlLabel
               key={index}
