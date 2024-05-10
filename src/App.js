392a26b7132205b2270a6d144c03f02aaae3d39d
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-05-10 21:25:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { styled } from '@mui/system';
 import Signup from "./Pages/Signup";
 import Recommender from "./Pages/Recommender";
 import Recommendation from "./Pages/Recommendation";
+import NavBar from "./Components/NavBar";
 import Movie from "./Assets/Movie.jpg";
+import Movie_Small from "./Assets/Movie_Small.jpg";
+import { useMediaQuery, useTheme } from '@mui/material';
 
-const Background = styled('div')({
-  backgroundImage: `url(${Movie})`,
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  height: '100vh',
-  width: '100%',
-  // background: 'rgba(255, 255, 255, 0.4)',
-  // boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
-  backdropFilter: 'blur(80px)',
-  WebkitBackdropFilter: 'blur(80px)',
-});
+function NavBarWithLocation() {
+  const location = useLocation();
+
+  return location.pathname !== "/" ? <NavBar /> : null;
+}
 
 function App() {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  
+  const Background = styled('div')({
+    backgroundImage: isSmallScreen ? `url(${Movie_Small})` : `url(${Movie})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    width: '100%',
+    // background: 'rgba(255, 255, 255, 0.4)',
+    // boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+    backdropFilter: 'blur(80px)',
+    WebkitBackdropFilter: 'blur(80px)',
+  });
+
+
   return (
     <Router>
       <Background>
+        <NavBarWithLocation />
         <Routes>
           <Route path="/Recommender" element={<Recommender />} />
           <Route path="/Recommendation" element={<Recommendation />} />
