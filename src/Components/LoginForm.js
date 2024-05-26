0f2836318eb20dbb726519dd17fff1189eeecd34
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-05-26 17:09:53 +0500

    Update LoginForm.js

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,9 +1,50 @@
 // LoginForm.js
-import { TextField, InputAdornment, IconButton, Button, CircularProgress } from '@material-ui/core';
+import { TextField, InputAdornment, IconButton, Button, CircularProgress, Link, Snackbar, Box } from '@material-ui/core';
 import { Visibility, VisibilityOff, LockOpen } from '@material-ui/icons';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { useState } from 'react';
+import { Alert } from '@material-ui/lab';
+import { Stack } from '@mui/material';
 
 function LoginForm({ email, setEmail, password, setPassword, showPassword, setShowPassword, handleLogin, formErrors, loading, classes }) {
+
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+  const handleForgotPassword = (event) => {
+    event.preventDefault(); // Prevent the default form submission
+  
+    const auth = getAuth();
+  
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent successfully
+        setSnackbarMessage('Reset email sent');
+        setOpenSnackbar(true);
+      })
+      .catch((error) => {
+        // An error occurred
+        setSnackbarMessage('Enter your email');
+        setOpenSnackbar(true);
+      });
+  };
+
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnackbar(false);
+  };
+  
     return (
+        <>
+        <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={handleCloseSnackbar} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+            <Alert onClose={handleCloseSnackbar} severity="info">
+              {snackbarMessage}
+            </Alert>
+        </Snackbar>
+        
         <form onSubmit={handleLogin}>
             <TextField
                 label="Email"
@@ -33,20 +74,34 @@ function LoginForm({ email, setEmail, password, setPassword, showPassword, setSh
                     ),
                 }}
             />
-            {loading ? (
-              <CircularProgress />
-            ) : (
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                startIcon={<LockOpen />}
-                onClick={handleLogin}
+
+            <Stack direction='column' spacing={2}>
+              <Link
+                  component="button"
+                  variant="body2"
+                  onClick={handleForgotPassword}
               >
-                Login
-              </Button>
-            )}
+                  Forgot Password?
+              </Link>
+
+              {loading ? (
+                <Box display='flex' justifyContent='center'>
+                <CircularProgress />
+                </Box>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                  startIcon={<LockOpen />}
+                  onClick={handleLogin}
+                >
+                  Login
+                </Button>
+              )}
+            </Stack>
         </form>
+        </>
     );
 }
 
