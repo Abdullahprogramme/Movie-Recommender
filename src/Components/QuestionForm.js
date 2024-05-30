0cb6678af958591074b79b0565acd740dcbc4d63
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-05-30 21:06:35 +0500

    Update QuestionForm.js

--- src/Components/QuestionForm.js
+++ src/Components/QuestionForm.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Checkbox, FormControlLabel, Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Checkbox, FormControlLabel, Box, Typography, Radio, RadioGroup } from '@mui/material';
 import { useTheme, useMediaQuery } from '@mui/material';
 
 export default function QuestionForm({ id, title, options, onOptionToggle }) {
@@ -7,9 +7,17 @@ export default function QuestionForm({ id, title, options, onOptionToggle }) {
   const firstColumnOptions = options.slice(0, optionsPerColumn);
   const secondColumnOptions = options.slice(optionsPerColumn);
 
-
   const theme = useTheme();
   const isScreenSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleRadioChange = (event) => { 
+    options.forEach(option => onOptionToggle(id, option, false));
+    
+    setSelectedOption(event.target.value);
+    onOptionToggle(id, event.target.value, true);
+  };
 
   return (
     <Box sx={{ marginTop: isScreenSmall ? 0 : 3 }}>
@@ -18,18 +26,31 @@ export default function QuestionForm({ id, title, options, onOptionToggle }) {
       </Typography>
       <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '10px' }}>
-          {firstColumnOptions.map((option, index) => (
-            <FormControlLabel
-              key={index}
-              sx={{ width: '100%' }}
-              control={
-                <Checkbox
-                  onChange={(event) => onOptionToggle(id, option, event.target.checked)}
+          {id === 'highRating' || id === 'criticalAcclaim' ? (
+            <RadioGroup value={selectedOption} onChange={handleRadioChange}>
+              {firstColumnOptions.map((option, index) => (
+                <FormControlLabel
+                  key={index}
+                  value={option}
+                  control={<Radio />}
+                  label={option}
                 />
-              }
-              label={option}
-            />
-          ))}
+              ))}
+            </RadioGroup>
+          ) : (
+            firstColumnOptions.map((option, index) => (
+              <FormControlLabel
+                key={index}
+                sx={{ width: '100%' }}
+                control={
+                  <Checkbox
+                    onChange={(event) => onOptionToggle(id, option, event.target.checked)}
+                  />
+                }
+                label={option}
+              />
+            ))
+          )}
         </Box>
         {options.length > optionsPerColumn && (
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
